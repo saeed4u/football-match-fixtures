940362bea2b1b0cfdb3f733164dc2e696978a508
= <=>
2019-07-17 21:02:51 +0300

    HomeFragment viewmodel

--- app/src/main/java/com/saeed/android/scoreline/ui/home/HomeFragment.kt
+++ app/src/main/java/com/saeed/android/scoreline/ui/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.lifecycle.Observer
 import com.saeed.android.scoreline.R
 import com.saeed.android.scoreline.ui.BaseFragment
 
@@ -18,9 +17,6 @@ class HomeFragment : BaseFragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        (viewModel as HomeViewModel).text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
--- app/src/main/java/com/saeed/android/scoreline/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/saeed/android/scoreline/ui/home/HomeViewModel.kt
@@ -3,12 +3,28 @@ package com.saeed.android.scoreline.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import com.saeed.android.scoreline.model.Competition
+import com.saeed.android.scoreline.model.Resource
+import com.saeed.android.scoreline.repo.CompetitionRepo
+import timber.log.Timber
 import javax.inject.Inject
 
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(private val competitionRepo: CompetitionRepo) :
+    ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+
+    private var competitionRefreshLiveData: MutableLiveData<Boolean> = MutableLiveData(true)
+    private val competitionListLiveData: LiveData<Resource<List<Competition>>>
+
+    init {
+        Timber.d("initiating ${this.javaClass.simpleName}")
+
+        competitionListLiveData = competitionRefreshLiveData.switchMap {
+            competitionRepo.getCompetitions(it)
+        }
     }
-    val text: LiveData<String> = _text
+
+    fun getCompetitionsList() = competitionListLiveData.value
+    fun postCompetition(refresh: Boolean = false) = competitionRefreshLiveData.postValue(refresh)
 }
