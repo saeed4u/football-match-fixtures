c34829f0aa94da827ddc1c0e04eb51c4d7098917
= <=>
2019-07-22 12:42:06 +0300

    added competition,team and match icons

--- app/src/main/java/com/saeed/android/scoreline/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/saeed/android/scoreline/ui/dashboard/DashboardFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
 import com.saeed.android.scoreline.R
 import com.saeed.android.scoreline.ui.BaseFragment
 
@@ -16,11 +14,6 @@ class DashboardFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        (viewModel as DashboardViewModel).text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 }
--- app/src/main/java/com/saeed/android/scoreline/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/saeed/android/scoreline/ui/dashboard/DashboardViewModel.kt
@@ -1,14 +1,8 @@
 package com.saeed.android.scoreline.ui.dashboard
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class DashboardViewModel @Inject constructor() : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
 }
